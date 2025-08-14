1fe8bc1ba6977d2516809077fc72285b07ce65ac
atharvashirodkar <atharva.shirodkar.12@gmail.com>
2025-08-14 19:17:13 +0530

    Added routes, port and sever

--- ems-backend/index.js
+++ ems-backend/index.js
@@ -3,3 +3,17 @@ import express from 'express';
 //rest object
 const app = express();
 
+//middleware
+
+//root
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+//port
+const PORT = 8080;
+
+//server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
