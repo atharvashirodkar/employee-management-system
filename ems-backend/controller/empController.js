56c7e00d5ff2c008b12e57ae9fa5f8a9cccd2b6c
atharvashirodkar <atharva.shirodkar.12@gmail.com>
2025-08-16 18:06:54 +0530

    Added getAllEmployee controller

---
+++ ems-backend/controller/empController.js
@@ -0,0 +1,29 @@
+import db from "../config/db.js";
+
+//GET ALL Employees
+const getAllEmployees = async (req, res) => {
+    try {
+        const data = await db.query('SELECT * FROM employees');
+        if (!data) {
+            return res.status(404).send({
+                success: false,
+                message: 'No employees found'
+            });
+        }
+        res.status(200).send({
+            success: true,
+            message: 'Employees fetched successfully',
+            totalEmployees: data[0].length,
+            data: data[0]
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error while fetching employees',
+            error
+        });
+    }
+}
+
+export default getAllEmployees;
