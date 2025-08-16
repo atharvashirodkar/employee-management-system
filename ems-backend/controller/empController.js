2b87db10fc46ceb8f9c025cd1a75c950289d7ef2
atharvashirodkar <atharva.shirodkar.12@gmail.com>
2025-08-16 18:09:05 +0530

    Added getEmployeeById controller

--- ems-backend/controller/empController.js
+++ ems-backend/controller/empController.js
@@ -24,6 +24,32 @@ const getAllEmployees = async (req, res) => {
             error
         });
     }
+};
+
+//GET Employee by ID
+const getEmployeeById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const data = await db.query('SELECT * FROM employees WHERE id = ?', [id]);
+        if (!data[0].length) {
+            return res.status(404).send({
+                success: false,
+                message: 'Employee not found'
+            });
+        }
+        res.status(200).send({
+            success: true,
+            message: 'Employee fetched successfully',
+            data: data[0][0]
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error while fetching employee',
+            error
+        });
+    }
 }
 
-export default getAllEmployees;
+export {getAllEmployees, getEmployeeById};
