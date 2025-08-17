89dfc23bf069c4e28a27b8814c974b293390de48
atharvashirodkar <atharva.shirodkar.12@gmail.com>
2025-08-17 23:45:26 +0530

    added routing (Home, Employees)

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import ListEmployee from "./pages/ListEmployee"
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/employees" element={<ListEmployee />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
