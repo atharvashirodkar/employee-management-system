86ca9818be23f393f2b4174dcc1370ec0a606ced
atharvashirodkar <atharva.shirodkar.12@gmail.com>
2025-08-18 14:21:09 +0530

    implement createEmployee API call

--- ems-frontend/src/services/EmployeeService.js
+++ ems-frontend/src/services/EmployeeService.js
@@ -12,3 +12,12 @@ export const getEmployees = async () => {
     }
 }
 
+export const createEmployee = async (employeeData) => {
+    try {
+        const response = await axios.post(API_BASE_URL, employeeData);
+        return response.data;
+    } catch (error) {
+        console.error("Error creating employee:", error);
+        throw error;
+    }
+}
