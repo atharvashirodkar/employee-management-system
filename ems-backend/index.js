a4551f87dd45a44ba90bd671193c2be441be087b
atharvashirodkar <atharva.shirodkar.12@gmail.com>
2025-08-18 21:25:26 +0530

    bind server to 0.0.0.0 for LAN access

--- ems-backend/index.js
+++ ems-backend/index.js
@@ -33,7 +33,7 @@ mySqlPool
         //MySQL
         console.log("MySQL DB connected");
         //server
-        app.listen(PORT, () => {
+        app.listen(PORT, '0.0.0.0',() => {
             console.log(`Server is running on port ${process.env.PORT}`);
         });
     }).catch((error) => {
