f2210c0d2ba7525c7cb7627e1c9e697fb24bca87
atharvashirodkar <atharva.shirodkar.12@gmail.com>
2026-06-18 21:58:05 +0530

    refactor: move database configuration to environment variables

--- ems-backend/config/db.js
+++ ems-backend/config/db.js
@@ -1,11 +1,13 @@
 import mysql2 from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
 //create connection
 const mySqlPool = mysql2.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'ems',
-}); 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
 
 export default mySqlPool;
